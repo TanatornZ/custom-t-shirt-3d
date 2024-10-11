27858049bd51f7ed533ce9becc0444b44b9dca54
TanatornZ <bank250145.bb@gmail.com>
2024-10-11 17:30:13 +0700

    feat: adjust camera

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@
 
 import { Canvas } from "@react-three/fiber";
 import Model from "./model";
+import { OrbitControls } from "@react-three/drei";
 
 export default function Home() {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
-      <Canvas>
+      <Canvas camera={{ position: [0, 4, 10] }}>
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <Model />
+        <Model key={"shirt"} />
+        <OrbitControls position={[10, 10, 10]} />
       </Canvas>
     </div>
   );
