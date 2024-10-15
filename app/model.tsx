297e7686894d80e9a4e981214f96aa7ce0b3fa43
TanatornZ <bank250145.bb@gmail.com>
2024-10-15 16:38:04 +0700

    feat: rotate with drag

--- app/model.tsx
+++ app/model.tsx
@@ -1,14 +1,25 @@
 // components/Model.js
 "use client";
-import { useGLTF, useScroll } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { useDrag } from "react-use-gesture";
 
-export default function Model(props) {
+export default function Model(props: any) {
+  const rotateY = useRef(0);
   const { scene } = useGLTF("/models/oversized_t-shirt/scene.gltf");
-  const scroll = useScroll();
-  useFrame(() => (scene.rotation.y = scroll.offset * Math.PI));
+  const drag = useDrag(
+    ({ offset: [x, y] }) => {
+      const xAis = x / 100;
 
-  return <primitive object={scene} {...props} />;
+      rotateY.current = xAis;
+    },
+    { pointerEvents: true }
+  );
+
+  useFrame(() => (scene.rotation.y = rotateY.current));
+
+  return <primitive object={scene} {...drag()} {...props} />;
 }
 
 // You can also preload the model to reduce loading times.
--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,27 @@
 
 import { Canvas } from "@react-three/fiber";
 import Model from "./model";
-import { ScrollControls } from "@react-three/drei";
+import { OrbitControls, ScrollControls } from "@react-three/drei";
 import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <div style={{ height: "100vh", width: "100vw" }}>
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <Suspense fallback={<div>loading...</div>}>
         <Canvas camera={{ position: [0, 4, 10], zoom: 5 }}>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
-          <ScrollControls pages={3}>
-            <mesh position={[-0, 1, 5.5]}>
-              <Model key={"shirt"} />
-            </mesh>
-          </ScrollControls>
+          <mesh position={[-0, 1, 5.5]}>
+            <Model key={"shirt"} />
+          </mesh>
         </Canvas>
       </Suspense>
     </div>
