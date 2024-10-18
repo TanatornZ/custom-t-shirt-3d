59c88db47bfc22dc38de506e1da6afdd66dc0da1
TanatornZ <bank250145.bb@gmail.com>
2024-10-18 22:48:55 +0700

    feat: change shrit color

--- app/components/Shirt.tsx
+++ app/components/Shirt.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-import { useGLTF, useTexture } from "@react-three/drei";
+import React, { useEffect } from "react";
+import {
+  Decal,
+  RenderTexture,
+  Text,
+  useGLTF,
+  useTexture,
+} from "@react-three/drei";
 
 export function Shirt() {
   const texture = useTexture("/texture/tiger-head.png");
   const { nodes, materials } = useGLTF("/models/shirt.glb");
+
+  useEffect(() => {}, []);
 
   return (
     <group dispose={null}>
@@ -14,19 +22,30 @@ export function Shirt() {
             receiveShadow
             geometry={nodes.Object_0.geometry}
             material={materials["Material.001"]}
-          />
-          <mesh geometry={nodes.Object_0_1.geometry} material={texture}>
-            {/* <meshBasicMaterial transparent opacity={0} />
+          >
+            {/* chang shirt color */}
+            <meshStandardMaterial color="red" />
             <Decal
-            debug
-            position={[0, 0.1, 1.2]}
-            rotation={[0, 0, 0]}
-            scale={[0.5, 0.5, 0.5]}
-            map={texture}
-            polygonOffsetFactor={-1}
-        > */}
-            {/* <meshBasicMaterial transparent map={texture} polygonOffset /> */}
-            {/* </Decal> */}
+              position={[0, 0.9, 0.75]}
+              rotation={[-0.4, Math.PI, 0]}
+              scale={[0.9, 0.25, 1]}
+            >
+              <meshStandardMaterial
+                roughness={1}
+                transparent
+                polygonOffset
+                polygonOffsetFactor={-1}
+              >
+                <RenderTexture attach="map">
+                  <color attach="background" args={["#242424"]} />
+                  <ambientLight intensity={Math.PI} />
+                  <directionalLight position={[10, 10, 5]} />
+                  <Text rotation={[0, Math.PI, 0]} fontSize={4} color="white">
+                    hello from drei
+                  </Text>
+                </RenderTexture>
+              </meshStandardMaterial>
+            </Decal>
           </mesh>
         </group>
       </group>
