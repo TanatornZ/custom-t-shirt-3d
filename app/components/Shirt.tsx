668c2b1280faa62c0bdab0a6625667f3395b5cee
TanatornZ <bank250145.bb@gmail.com>
2024-10-19 22:54:36 +0700

    chore: split mesh for color and texture

--- app/components/Shirt.tsx
+++ app/components/Shirt.tsx
@@ -3,15 +3,20 @@ import {
   Decal,
   RenderTexture,
   Text,
+  useAspect,
   useGLTF,
   useTexture,
 } from "@react-three/drei";
 
 export function Shirt() {
-  const texture = useTexture("/texture/tiger-head.png");
+  const texture = useTexture("/texture/basketball.png");
   const { nodes, materials } = useGLTF("/models/shirt.glb");
 
-  useEffect(() => {}, []);
+  const scale = useAspect(
+    512, // Pixel-width
+    512, // Pixel-height
+    1 // Optional scaling factor
+  );
 
   return (
     <group dispose={null}>
@@ -25,10 +30,30 @@ export function Shirt() {
           >
             {/* chang shirt color */}
             <meshStandardMaterial color="red" />
+          </mesh>
+
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Object_0_1.geometry}
+            material={materials["printable.001"]}
+          >
             {/* TODO: display texture image */}
-            {/* <Decal position={[0, 0, 0]} rotation={[0, 0, 0]} scale={[1, 1, 1]}>
-              <meshBasicMaterial map={texture} />
-            </Decal> */}
+            <meshBasicMaterial transparent opacity={0} />
+            <Decal
+              debug
+              position={[0, -0.1, 1.2]}
+              rotation={[0, 0, 0]}
+              scale={0.2}
+            >
+              <meshBasicMaterial
+                transparent
+                toneMapped={false}
+                map={texture}
+                polygonOffset
+                polygonOffsetFactor={-1}
+              />
+            </Decal>
           </mesh>
         </group>
       </group>
