8426c7274d3b1d6af9b4009a68ab405a11518e01
TanatornZ <bank250145.bb@gmail.com>
2024-11-07 11:16:31 +0700

    feat: aspect ratio texture

--- app/components/Shirt.tsx
+++ app/components/Shirt.tsx
@@ -13,6 +13,7 @@ export function Shirt({
 }) {
   const { nodes, materials } = useGLTF("/models/shirt_baked_collapsed.glb");
   const loader = new TextureLoader();
+  const [decalScale, setDecalScale] = useState([0.2, 0.2, 1]);
 
   const [texture, setTexture] = useState(
     loader.load("/texture/white-background.jpg")
@@ -20,7 +21,25 @@ export function Shirt({
 
   useEffect(() => {
     if (shirtTexture) {
-      const shirt = loader.load(shirtTexture);
+      const shirt = loader.load(shirtTexture, (imageData) => {
+        let original_height = imageData.source.data.height;
+        let default_scale = 0.25;
+        let original_width = imageData.source.data.width;
+
+        if (original_height > original_width) {
+          let scaling_factor = default_scale / original_height;
+
+          let new_width = original_width * scaling_factor;
+
+          setDecalScale([new_width, default_scale, 1]);
+        } else {
+          let scaling_factor = default_scale / original_width;
+
+          let new_height = original_height * scaling_factor;
+
+          setDecalScale([default_scale, new_height, 1]);
+        }
+      });
       setTexture(shirt);
     }
   }, [shirtTexture]);
@@ -39,7 +58,11 @@ export function Shirt({
         geometry={nodes.T_Shirt_male.geometry}
         material={materials.lambert1}
       >
-        <Decal position={[0, 0.04, 0.1]} rotation={[0, 0, 0]} scale={0.2}>
+        <Decal
+          position={[0, 0.05, 0.1]}
+          rotation={[0, 0, 0]}
+          scale={decalScale}
+        >
           <meshBasicMaterial
             map={texture}
             transparent
